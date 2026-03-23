23cc0900c8b94dd43b3028f47127e4b5c486305e
bubber28 <rubemardigital@gmail.com>
2026-03-23 02:44:54 +0000

    Fix: Restaurando estrutura mínima e main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,4 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+const App = () => <div><h1>I Love Delícias</h1><p>Em manutenção para deploy...</p></div>;
+ReactDOM.createRoot(document.getElementById("root")!).render(<React.StrictMode><App /></React.StrictMode>);
